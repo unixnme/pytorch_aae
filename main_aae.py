96fd86a1e20e5b57b032ee9277c28f85353520d9
Unix&Me <unixnme@gmail.com>
2019-11-01 22:00:50 -0700

    remove tf; option to select device

--- main_aae.py
+++ main_aae.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
-from logger import Logger
 
+device = 'cpu'
 
 # MNIST Dataset 
 dataset = dsets.MNIST(root='./data', 
@@ -23,7 +23,7 @@ def to_np(x):
 
 def to_var(x):
     if torch.cuda.is_available():
-        x = x.cuda()
+        x = x.to(device)
     return Variable(x)    
 
 
@@ -54,7 +54,7 @@ class P_net(nn.Module):
         x = F.relu(x)
         x = F.dropout(self.lin2(x), p=0.25, training=self.training)
         x = self.lin3(x)
-        return F.sigmoid(x)
+        return torch.sigmoid(x)
 
 # Discriminator
 class D_net_gauss(nn.Module):  
@@ -68,17 +68,14 @@ class D_net_gauss(nn.Module):
         x = F.relu(x)
         x = F.dropout(self.lin2(x), p=0.2, training=self.training)
         x = F.relu(x)
-        return F.sigmoid(self.lin3(x))        
+        return torch.sigmoid(self.lin3(x))
         
 
 EPS = 1e-15
 z_red_dims = 120
-Q = Q_net(784,1000,z_red_dims).cuda()
-P = P_net(784,1000,z_red_dims).cuda()
-D_gauss = D_net_gauss(500,z_red_dims).cuda()
-
-# Set the logger
-logger = Logger('./logs/z_120_fixed_LR_2')
+Q = Q_net(784,1000,z_red_dims).to(device)
+P = P_net(784,1000,z_red_dims).to(device)
+D_gauss = D_net_gauss(500,z_red_dims).to(device)
 
 # Set learning rates
 gen_lr = 0.0001
@@ -123,12 +120,13 @@ for step in range(total_step):
     ## true prior is random normal (randn)
     ## this is constraining the Z-projection to be normal!
     Q.eval()
-    z_real_gauss = Variable(torch.randn(images.size()[0], z_red_dims) * 5.).cuda()
+    z_real_gauss = Variable(torch.randn(images.size()[0], z_red_dims) * 1.).to(device)
     D_real_gauss = D_gauss(z_real_gauss)
 
     z_fake_gauss = Q(images)
     D_fake_gauss = D_gauss(z_fake_gauss)
 
+    D_acc = ((D_real_gauss > 0.5).sum() + (D_fake_gauss < 0.5).sum()) / (len(D_real_gauss) + len(D_fake_gauss) + EPS)
     D_loss = -torch.mean(torch.log(D_real_gauss + EPS) + torch.log(1 - D_fake_gauss + EPS))
 
     D_loss.backward()
@@ -146,34 +144,10 @@ for step in range(total_step):
 
     
     if (step+1) % 100 == 0:
-        # print ('Step [%d/%d], Loss: %.4f, Acc: %.2f' 
-        #        %(step+1, total_step, loss.data[0], accuracy.data[0]))
+        print ('Step [%d/%d], Loss: %.4f, Acc: %.4f%%'
+               %(step+1, total_step, D_loss.item(), D_acc*100))
 
-        #============ TensorBoard logging ============#
-        # (1) Log the scalar values
-        info = {
-            'recon_loss': recon_loss.data[0],
-            'discriminator_loss': D_loss.data[0],
-            'generator_loss': G_loss.data[0]
-        }
 
-        for tag, value in info.items():
-            logger.scalar_summary(tag, value, step+1)
-
-        # (2) Log values and gradients of the parameters (histogram)
-        for net,name in zip([P,Q,D_gauss],['P_','Q_','D_']): 
-            for tag, value in net.named_parameters():
-                tag = name+tag.replace('.', '/')
-                logger.histo_summary(tag, to_np(value), step+1)
-                logger.histo_summary(tag+'/grad', to_np(value.grad), step+1)
-
-        # (3) Log the images
-        info = {
-            'images': to_np(images.view(-1, 28, 28)[:10])
-        }
-
-        for tag, images in info.items():
-            logger.image_summary(tag, images, step+1)
 
 #save the Encoder
 torch.save(Q.state_dict(),'Q_encoder_weights.pt')
